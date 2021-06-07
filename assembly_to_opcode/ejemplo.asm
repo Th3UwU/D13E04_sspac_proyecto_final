1c59ea1599a72ee4fd17cf650c7a14d009f6442d
Th3UwU <84390450+Th3UwU@users.noreply.github.com>
2021-06-07 10:30:41 -0500

    Add files via upload

---
+++ assembly_to_opcode/ejemplo.asm
@@ -0,0 +1,11 @@
+# Comentario
+add $1, $2, $20
+addi $4, $7, 24
+slti $4, $7, 24
+sub $5, $4, $21
+and $8, $4, $24
+
+# Otro comentario
+or $10, $10, $25
+slt $5, $10, $26
+nop
---
+++ assembly_to_opcode/main.py
@@ -0,0 +1,118 @@
+import re
+
+def main():
+	file_name = input("Ingresa el nombre del archivo --> "); print("")
+	#file_name = "example.asm"
+	try:
+		file = open(file_name, "r")
+		lines = file.readlines()
+		file.close()
+
+		open(file_name+"_converted.txt", "w").close ## borrar contenido
+		new_file = open(file_name+"_converted.txt", "a")
+
+		for i in lines:
+			op_code = parse(i[:-1])
+
+			if (op_code != ""): # Solo si es opcode
+				print(op_code, end="\n") ## mostrar en consola
+				new_file.write(op_code+"\n") ## escribir en el archivo
+
+		new_file.close()
+		print("\nTu archivo ha sido convertido correctamente!!\n")
+		input()
+
+	except:
+		print("Error 404 !")
+		input()
+
+	"""
+	assembly_code = "add $12, $3, $4"
+	op_code = parse(assembly_code)
+	print(op_code)
+	"""
+
+def parse(code):
+	# Default op code value
+	op_code = ""
+
+	# IS NOP
+	if (code == "nop"):
+		return (32*"0")
+
+	# TIPO R / 000000
+	R_TYPE = {
+		"add": "100000",
+		"sub": "100010",
+		"or": "100101",
+		"and": "100100",
+		"slt": "101010"
+	}
+	for i in R_TYPE:
+		if i+" $" in code: # Por ejemplo --> "add $" y no confundir con "addi $"
+			op_code = "000000"
+			shamt = "00000"
+			op_code_a = i
+			# Function
+			funct = R_TYPE[i]
+
+			# GET: dest
+			pattern = op_code_a+" \$(.+?)," # patron a buscar
+			dest_a = re.search(pattern, code).group(1)
+			dest = format(int(dest_a), "05b") # to 5 bits binary
+			dest_a = "\$"+dest_a # concatenar '$'
+
+			# GET: src1
+			pattern = op_code_a+" "+dest_a+", \$(.*)," # patron a buscar
+			src1_a = re.search(pattern, code).group(1)
+			src1 = format(int(src1_a), "05b") # to 5 bits binary
+			# add '$'
+			src1_a = "\$"+src1_a # concatenar '$'
+
+			# GET: src2
+			pattern = op_code_a+" "+dest_a+", "+src1_a+", \$(.*)" # patron a buscar
+			src2_a = re.search(pattern, code).group(1)
+			src2 = format(int(src2_a), "05b") # to 5 bits binary
+			src2_a = "\$"+src2_a # concatenar '$'
+
+			return op_code+src1+src2+dest+shamt+funct
+
+	# TIPO I
+	I_TYPE = {
+		"addi": "001000",
+		"slti": "001010",
+		"andi": "001100",
+		"ori": "001101",
+		"sw": "101011",
+		"lw": "100011",
+	}
+
+	for i in I_TYPE:
+		if i in code:
+			op_code = I_TYPE[i]
+			op_code_a = i
+
+			# GET: rt
+			pattern = op_code_a+" \$(.+?)," # patron a buscar
+			rt_a = re.search(pattern, code).group(1)
+			rt = format(int(rt_a), "05b") # to 5 bits binary
+			rt_a = "\$"+rt_a # concatenar '$'
+
+			# GET: rs
+			pattern = op_code_a+" "+rt_a+", \$(.+?)," # patron a buscar
+			rs_a = re.search(pattern, code).group(1)
+			rs = format(int(rs_a), "05b") # to 5 bits binary
+			rs_a = "\$"+rs_a # concatenar '$'
+
+			# GET: inmediate
+			pattern = op_code_a+" "+rt_a+", "+rs_a+", (.*)" # patron a buscar
+			inmediate_a = re.search(pattern, code).group(1)
+			inmediate = format(int(inmediate_a), "016b") # to 16 bits binary
+
+			return op_code+rs+rt+inmediate
+
+	return "" # Se omite
+
+
+if __name__ == "__main__":
+	main()
